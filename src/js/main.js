a103e4eed787c82a0f3c72ca0285e543a2d76a14
John Matula <john@johnmatu.la>
2020-06-07 21:33:13 -0400

    Add CSS custom property manipulation in JS

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,28 @@
-console.log("Hello world?");
+let root = document.documentElement;
+document.body.classList.remove('js--disabled')
+root.style.setProperty('--width-page', window.innerWidth);
+
+window.addEventListener("resize", () => {
+  root.style.setProperty('--width-page', window.innerWidth);
+});
+/*
+window.addEventListener("scroll", () => {
+  root.style.setProperty('--scroll-y', window.scrollY)
+});
+*/
+
+let lastKnownScrollPosition = 0;
+let ticking = false;
+
+window.addEventListener('scroll', function(e) {
+  lastKnownScrollPosition = window.scrollY;
+
+  if (!ticking) {
+    window.requestAnimationFrame(function() {
+    root.style.setProperty('--scroll-y', lastKnownScrollPosition);
+      ticking = false;
+    });
+
+    ticking = true;
+  }
+});
