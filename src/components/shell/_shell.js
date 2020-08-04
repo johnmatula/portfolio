ff6c43167b28a298bbe7cdf0acfb4c142b5b3d97
John Matula <john@johnmatu.la>
2020-08-03 20:56:28 -0400

    Reorganize src

--- src/components/shell/_shell.js
+++ src/components/shell/_shell.js
@@ -5,11 +5,6 @@ root.style.setProperty('--width-page', window.innerWidth);
 window.addEventListener("resize", () => {
   root.style.setProperty('--width-page', window.innerWidth);
 });
-/*
-window.addEventListener("scroll", () => {
-  root.style.setProperty('--scroll-y', window.scrollY)
-});
-*/
 
 let lastKnownScrollPosition = 0;
 let ticking = false;
