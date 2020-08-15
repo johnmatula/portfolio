f865935fb5e19ff843fef7967ee4f2cbd2732a9b
John Matula <john@johnmatu.la>
2020-08-15 13:17:45 -0400

    Increment support files

--- src/supporting/generated/js/main.js
+++ src/supporting/generated/js/main.js
@@ -1,5 +1,36 @@
 "use strict";
 
+function nav() {
+  return {
+    submenu: false,
+    openSubmenu: function openSubmenu(menu) {
+      this.submenu = menu;
+    },
+    tester: function tester(e) {
+      console.log(e);
+      console.log(this);
+    },
+    isSubmenuOpen: function isSubmenuOpen(menu) {
+      return menu === this.submenu;
+    },
+    closeSubmenus: function closeSubmenus() {
+      this.submenu = false;
+    },
+    listenForEscape: function listenForEscape() {
+      var _this = this;
+
+      // Use ES6 notation so that `this` refers to this component
+      // (a proud moment that John remembered this differentiation)
+      window.addEventListener('keypress', function (e) {
+        if (e.code === 'Escape') {
+          _this.closeSubmenus();
+        }
+      });
+    }
+  };
+}
+"use strict";
+
 var root = document.documentElement;
 document.body.classList.remove('js--disabled');
 root.style.setProperty('--width-page', window.innerWidth);
