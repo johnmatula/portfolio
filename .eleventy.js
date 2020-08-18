3e1e16735de852e6f4498f0f513dc2c3a559d0db
John Matula <john@johnmatu.la>
2020-08-18 14:30:55 -0400

    Add brand fonts and supporting stylesheets

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ module.exports = (function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "src/supporting/generated/css": "css" });
   eleventyConfig.addPassthroughCopy({ "src/supporting/generated/js": "js" });
   eleventyConfig.addPassthroughCopy({ "src/supporting/vendor-js": "js" });
+  eleventyConfig.addPassthroughCopy({ "src/supporting/fonts": "fonts" });
   eleventyConfig.addPassthroughCopy("src/img");
 
   return {
