2424347bf70d1a89a33178b9ae14755c0db4c2c8
John Matula <john@johnmatu.la>
2020-08-21 10:01:12 -0400

    Increment supporting CSS files

--- src/supporting/generated/js/main.js
+++ src/supporting/generated/js/main.js
@@ -1,6 +1,6 @@
 "use strict";
 
-function nav() {
+function sidebar() {
   return {
     submenu: false,
     openSubmenu: function openSubmenu(menu) {
@@ -29,13 +29,19 @@ function nav() {
     }
   };
 }
+
+window.addEventListener("load", function () {
+  document.body.classList.add("js--ready");
+});
 "use strict";
 
 var root = document.documentElement;
 document.body.classList.remove('js--disabled');
 root.style.setProperty('--width-page', window.innerWidth);
+updateEnvRemValues();
 window.addEventListener("resize", function () {
   root.style.setProperty('--width-page', window.innerWidth);
+  updateEnvRemValues();
 });
 var lastKnownScrollPosition = 0;
 var ticking = false;
@@ -49,6 +55,18 @@ window.addEventListener('scroll', function (e) {
     });
     ticking = true;
   }
-});
+}); // Take in the pixel-value string as defined in the environment variable and
+// placed into a custom property, and convert it to rems so that it works
+// nicely with responsive transforms in CSS.
+
+function updateEnvRemValues() {
+  var computedStyle = getComputedStyle(document.documentElement);
+  var envList = ['--env-safe-top', '--env-safe-right', '--env-safe-bottom', '--env-safe-left'];
+  envList.forEach(function (env) {
+    var value = parseInt(computedStyle.getPropertyValue(env + '-pixels'), 10);
+    var remValue = value / computedStyle.getPropertyValue('--rem-base');
+    root.style.setProperty(env, remValue);
+  });
+}
 
 //# sourceMappingURL=main.js.map
