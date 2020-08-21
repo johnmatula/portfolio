a59eb47413315c27d9a1149789996f94d1c0d5d5
John Matula <john@johnmatu.la>
2020-08-21 09:57:15 -0400

    Move shell from components to templates

---
+++ src/templates/shell/_shell.js
@@ -0,0 +1,40 @@
+let root = document.documentElement;
+document.body.classList.remove('js--disabled')
+root.style.setProperty('--width-page', window.innerWidth);
+updateEnvRemValues();
+
+window.addEventListener("resize", () => {
+  root.style.setProperty('--width-page', window.innerWidth);
+  updateEnvRemValues();
+});
+
+var lastKnownScrollPosition = 0;
+var ticking = false;
+
+window.addEventListener('scroll', function(e) {
+  lastKnownScrollPosition = window.scrollY;
+
+  if (!ticking) {
+    window.requestAnimationFrame(function() {
+      root.style.setProperty('--scroll-y', lastKnownScrollPosition);
+      ticking = false;
+    });
+
+    ticking = true;
+  }
+});
+
+// Take in the pixel-value string as defined in the environment variable and
+// placed into a custom property, and convert it to rems so that it works
+// nicely with responsive transforms in CSS.
+function updateEnvRemValues() {
+  const computedStyle = getComputedStyle(document.documentElement)
+  const envList = ['--env-safe-top','--env-safe-right','--env-safe-bottom','--env-safe-left'];
+
+  envList.forEach((env) => {
+    var value = parseInt(computedStyle.getPropertyValue(env + '-pixels'), 10);
+    var remValue = value / computedStyle.getPropertyValue('--rem-base');
+
+    root.style.setProperty(env, remValue);
+  })
+}
