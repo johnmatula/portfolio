cc29e4e5ec89ec596683a54d8b0d0926514f045b
John Matula <john@johnmatu.la>
2020-11-11 15:08:13 -0500

    Update async responsive image macro

--- .eleventy.js
+++ .eleventy.js
@@ -16,12 +16,13 @@ module.exports = (function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "src/supporting/vendor-js": "js" });
   eleventyConfig.addPassthroughCopy({ "src/supporting/fonts": "fonts" });
   eleventyConfig.addPassthroughCopy({ "src/supporting/images": "img" }); // unnecessary???
+  eleventyConfig.addPassthroughCopy({ "src/supporting/favicon": "/" });
 
   eleventyConfig.addNunjucksFilter("is_string", function(obj) {
     return typeof obj === "string";
   });
 
-  eleventyConfig.addNunjucksAsyncShortcode("responsiveImage", async function(src, alt, outputFormat = "jpeg") {
+  eleventyConfig.addNunjucksAsyncShortcode("responsiveImage", async function(src, alt, classList="responsive--thirdwide", container = "figure", outputFormat = "jpeg") {
     if(alt === undefined) {
       throw new Error(`No alt text provided on responsiveImage with source ${src}`);
     }
@@ -33,9 +34,19 @@ module.exports = (function(eleventyConfig) {
       outputDir: "./dist/img/",
     });
     let lowestSrc = stats[outputFormat][0];
+    let highestSrc = stats[outputFormat][stats[outputFormat].length - 1];
     let sizes = "500w 100vw"; // Make sure you customize this!
+    //console.log(stats);
 
-    return `<picture>
+    let containerStart, containerEnd = "";
+
+    if(container !== null) {
+      containerStart = `<${container} class="responsive ${classList}">`;
+      containerEnd = `</${container}>`;
+    }
+
+    return `${containerStart}
+    <a href="${highestSrc.url}" class="responsive__link" itemprop="contentUrl" data-size="${highestSrc.width}x${highestSrc.height}"><picture class="responsive__picture">
       ${Object.values(stats).map(imageFormat => {
         return `  <source type="image/${imageFormat[0].format}" srcset="${imageFormat.map(entry => `${entry.url} ${entry.width}w`).join(", ")}" sizes="${sizes}">`;
       }).join("\n")}
@@ -43,8 +54,9 @@ module.exports = (function(eleventyConfig) {
           src="${lowestSrc.url}"
           width="${lowestSrc.width}"
           height="${lowestSrc.height}"
-          alt="${alt}">
-      </picture>`;
+          alt="${alt}"
+          class="responsive__img">
+      </a></picture>${containerEnd}`;
   });
 
   eleventyConfig.addLayoutAlias("case-study", "../templates/case-study/case-study.njk");
